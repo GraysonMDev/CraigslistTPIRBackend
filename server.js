2c38fcdfe111008853d85cd6b5ddb38a35843bc4
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2022-02-14 18:11:00 -0600

    created working backend

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+app.use(cors())
+const http = require('http');
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = require("socket.io")(server, 
+    { 
+    cors: {    
+      origin: "*",    
+      methods: ["GET", "POST"]  
+    }});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on("sendMessage", message => {
+      console.log(message)
+      socket.emit("serverMessage", "hello from the other side")
+  })
+});
+
+app.get('/', function (req, res, next){
+    res.json("hello")
+})
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+}); 
